2547fea826fcc6673d56411c27be0ec99177d66a
AudreyLuce <AudreyLuce@users.noreply.github.com>
2019-04-23 01:38:10 -0400

    Expanded Challenge 3 files for Final

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+    public Text winText;
+    public ParticleSystem mainPS;
+    public ParticleSystem distantPS;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+
+
+        var main = mainPS.main;
+        var distant = distantPS.main;
+
+        if (winText.text.Equals(""))
+        {
+
+        }
+        else
+        {
+            scrollSpeed = -6;
+            main.simulationSpeed = 50;
+            distant.simulationSpeed = 50;
+        }
+    }
+}
